d92f7fe25ec98b7cd9c595530d9592575df9de11
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-15 15:51:43 +0530

    objects

--- dist/classes.js
+++ dist/classes.js
@@ -1,2 +1,17 @@
 "use strict";
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.balance = balance;
+        this.owner = owner;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+}
+let account = new Account(1, 'Mosh', 0);
+account.deposit(100);
+console.log(account instanceof Account);
 //# sourceMappingURL=classes.js.map
