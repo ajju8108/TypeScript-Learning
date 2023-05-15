3aa893c4abbb1518a965f96bad4e3df3d0636ce1
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-15 11:42:31 +0530

    built in types

--- dist/first.js
+++ dist/first.js
@@ -1,6 +1,17 @@
-"use strict";
+/* "use strict";
 let age = 20;
 if (age < 50)
     age += 10;
-console.log(age);
+console.log(age); */
 //# sourceMappingURL=first.js.map
+
+/* Built In Types */
+/* let sales: number = 123456789
+let course: string = 'Typescript'
+let is_published: boolean = true */
+
+// We dont need to provide the types 
+let sales = 123456789
+let course = 'Typescript'
+let is_published = true
+let level // type is any
