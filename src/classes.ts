9e01d091dc420b4ca6711f80cd6734dce0662e87
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-15 15:56:02 +0530

    read only

--- src/classes.ts
+++ src/classes.ts
@@ -1,7 +1,8 @@
 class Account {
-    id: number;
+    readonly id: number;
     owner: string;
     balance: number;
+    nickname?: string; // this will make optional parameter 
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
@@ -15,8 +16,13 @@ class Account {
     }
 }
 let account = new Account(1, 'Mosh', 0)
-account.deposit(100)
+
+
+// account.deposit(100)
 // console.log(account.balance);
 // console.log(account);
 // console.log(typeof account);
-console.log(account instanceof Account);
+// console.log(account instanceof Account);
+
+// Readonly
+// account.id = 0
