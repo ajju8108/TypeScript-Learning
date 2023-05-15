9d118a50b654644af41992843d2ff71cbe8679a4
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-15 17:02:30 +0530

    inheritance

--- src/classes.ts
+++ src/classes.ts
@@ -2,7 +2,7 @@
     // readonly id: number;
     // owner: string;
     // private _balance: number;
-    nickname?: string; // this will make optional parameter 
+    nickname?: string; // this will make optional parameter
 
     constructor(
         public readonly id: number,
@@ -44,10 +44,61 @@ console.log(account.balance);
 // Readonly
 // account.id = 0 */
 
-class SeatAssignment {
+/* class SeatAssignment {
     //Index Signature Property
     [seatNumber: string]: string;
 }
 let seats = new SeatAssignment();
 seats.A1 = 'Mosh'
-seats.A2 = 'John'
+seats.A2 = 'John' */
+
+
+
+//Static Members
+
+/* class Ride {
+    private static _activeRides: number = 0;
+    start() {
+        Ride._activeRides++;
+    }
+    stop() {
+        Ride._activeRides--;
+    }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+
+let ride1 = new Ride();
+ride1.start()
+
+let ride2 = new Ride();
+ride1.start()
+
+console.log(Ride.activeRides); */
+
+
+//Inheritance
+class Person {
+    constructor(public firstName: string, public lastName: string) {
+
+    }
+    get fullName() {
+        return this.firstName + ' ' + this.lastName
+    }
+    walk() {
+        console.log('walking');
+
+    }
+}
+
+class Student extends Person {
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        super(firstName, lastName);
+    }
+    takeTestI() {
+        console.log('Taking a test');
+
+    }
+}
+let student = new Student(1, 'John', 'gmail')
