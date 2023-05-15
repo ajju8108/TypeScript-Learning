747f22aaaec46ea9fb71fa67ebc2db3a70582cec
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-15 11:59:17 +0530

    arrays

--- dist/first.js
+++ dist/first.js
@@ -1,17 +1,3 @@
-/* "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age); */
+"use strict";
+let numbers = [1, 2];
 //# sourceMappingURL=first.js.map
-
-/* Built In Types */
-/* let sales: number = 123456789
-let course: string = 'Typescript'
-let is_published: boolean = true */
-
-// We dont need to provide the types 
-let sales = 123456789
-let course = 'Typescript'
-let is_published = true
-let level // type is any
--- src/first.ts
+++ src/first.ts
@@ -1,4 +1,36 @@
-let age: number = 20; 
-if(age < 50)
+/* "use strict";
+let age = 20;
+if (age < 50)
     age += 10;
-console.log(age);
+console.log(age); */
+//# sourceMappingURL=first.js.map
+
+/* Built In Types */
+/* let sales: number = 123456789
+let course: string = 'Typescript'
+let is_published: boolean = true */
+
+// We dont need to provide the types 
+/* let sales = 123456789
+let course = 'Typescript'
+let is_published = true */
+//let level // type is any
+
+// ANY TYPE
+
+/* let level;
+level = 1
+level = 'a'
+console.log(level);
+
+function render(document: any) {
+    console.log(document);
+} */
+
+// Arrays
+// let numbers = [1, 2]
+/* let numbers = [] // any type
+let numbers2 : number[] = []
+numbers2.forEach(n => { */
+    
+});
